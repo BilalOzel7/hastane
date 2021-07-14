4d9b124742787e272025a3d7bfbacfbc6f0fe9df
BilalOzel7 <mbozel07@gmail.com>
2021-07-15 00:54:31 +0300

    hastane
    hastane

---
+++ Hastane/DataAccess/Concrete/EntityFramework/EfDoktorDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfDoktorDal : EfEntityRepositoryBase<Doktor, HastaneContext>, IDoktorDal
+    {
+        
+    }
+}
---
+++ Hastane/DataAccess/Concrete/EntityFramework/HastaneContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+   public class HastaneContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-V3FGGNT\SQLEXPRESS;Database=Hastane;Trusted_Connection=true");
+        }
+
+        public DbSet<Doktor> Doktorlar { get; set; }
+        public DbSet<TibbiBirim> TibbiBirim { get; set; }
+    }
+}
